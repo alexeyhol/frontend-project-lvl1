86c959a23a57d044524b7db75d04a79b449b5108
Alexey Kholodov <alexeyhol@yandex.ru>
2021-01-01 19:08:26 +0700

    Readline-sync added

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
+import readlineSync from 'readline-sync'
+
 console.log('Welcome to the Brain Games!')
+
+const userName = readlineSync.question('May I have your name?\n')
+
+console.log('Hello, ' + userName)
